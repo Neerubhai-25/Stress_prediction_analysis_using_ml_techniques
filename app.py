d20a2ddb2a3bf2836bffc3bfc5cc1938e16036c0
neerajsai-25 <228813463+Neerubhai-25@users.noreply.github.com>
2026-04-22 00:16:00 +0530

    Remove unused import for 'validate' from attr

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from attr import validate
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
